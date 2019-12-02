04214eaf63a1b9e8262098bb6299650d599da784
Daniil Barabash <daniil.barabash@jumpshot.com>
2019-12-02 21:37:27 +0100

    add top level export

--- src/main/scala/webapp/Main.scala
+++ src/main/scala/webapp/Main.scala
@@ -1,12 +1,19 @@
 package webapp
 
+import org.scalajs.dom
+import dom.document
+
+import scala.scalajs.js.annotation.JSExportTopLevel
+
 /**
   * Template for Scala.js application
   */
 object Main {
 
-  def main(args: Array[String]): Unit = {
+  @JSExpportTopLevel("list")
+  var l = List(1,2,3)
 
+  def main(args: Array[String]): Unit = {
     println("Hello from Scala.js!")
   }
 }
